b7d7286e59f84eac5ff1c219644cc358e6bf3f5c
ReenExe <ipourri@gmail.com>
2017-01-22 17:59:51 +0200

    [+] gulp, pagination

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    eol = require('gulp-eol');
+
+gulp.task('default', function() {
+});
+
+/*** MAIN ***/
+gulp.task('main', function () {
+    return gulp.src(['sass/main.sass'])
+        .pipe(sass())
+        .pipe(concatCss('main.css'))
+        .pipe(eol('\n'))
+        .pipe(gulp.dest('css/'));
+});
+
+/*** LIST ***/
+gulp.task('list', function () {
+    return gulp.src(['sass/list.sass'])
+        .pipe(sass())
+        .pipe(concatCss('list.css'))
+        .pipe(eol('\n'))
+        .pipe(gulp.dest('css/'));
+});
+
+
+/*** BUILD ***/
+gulp.task('build', ['list', 'main'], function () {
+    return gulp.src(['css/main.css', 'css/list.css'])
+        .pipe(concatCss('build.min.css'))
+        .pipe(cleanCSS())
+        .pipe(minifyCss())
+        .pipe(eol('\n'))
+        .pipe(gulp.dest('css/'));
+});
+
+/*** WATCH ***/
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.sass', ['main', 'list']);
+});
