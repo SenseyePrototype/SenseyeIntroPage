117ff530a121419fb1f82dc459328c09c0638f5a
Golars <titorens@gmail.com>
2017-02-12 19:57:41 +0200

    [+] city in profile, skills page #9

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ $('.modal-popup .close, .modal-popup .modal-bg').on('click', function(){
     $('body').removeClass('modal-open');
 });
 
+/**
+ * Init Skills search
+ */
+$(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+        data: {
+            "CSS": null,
+            "html": null,
+            "ILoveGolars": null,
+            "git": 'https://git-scm.com/images/logos/downloads/Git-Icon-Black.png'
+        },
+        limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    });
+});
+
+$(document).ready(function() {
+    $('select').material_select();
+});
+
